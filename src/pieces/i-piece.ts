b16d3d2b07d065055d0527cb36b2ca9924539f4f
Grímur Kristinsson <grimurk@gmail.com>
2020-02-09 01:34:54 +0000

    Make I piece start flat

--- src/pieces/i-piece.ts
+++ src/pieces/i-piece.ts
@@ -13,21 +13,22 @@ export class IPiece extends Piece {
     const x = this.col;
     const y = this.row;
     return  [
-      {x: x + 1, y},
+      {x, y: y + 1},
       {x: x + 1, y: y + 1},
-      {x: x + 1, y: y + 2},
-      {x: x + 1, y: y + 3}
+      {x: x + 2, y: y + 1},
+      {x: x + 3, y: y + 1}
     ];
+    
   }
 
   protected getRightCoords() {
     const x = this.col;
     const y = this.row;
     return  [
-      {x, y: y + 1},
+      {x: x + 1, y},
       {x: x + 1, y: y + 1},
-      {x: x + 2, y: y + 1},
-      {x: x + 3, y: y + 1}
+      {x: x + 1, y: y + 2},
+      {x: x + 1, y: y + 3}
     ];
   }
 
