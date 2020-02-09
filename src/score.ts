768c0d0592dfd4ce13c347a99736a5b0b9b2af55
Grímur Kristinsson <grimurk@gmail.com>
2020-02-09 15:50:52 +0000

    Update level and lines

--- src/score.ts
+++ src/score.ts
@@ -3,20 +3,22 @@ export class Score {
   level = 1;
 
   private scoreEl: HTMLSpanElement;
+  private levelEl: HTMLSpanElement;
+  private linesEl: HTMLSpanElement;
   private score!: number;
   private lines = 0;
 
   constructor() {
     this.scoreEl = document.getElementById('score') as HTMLSpanElement;
+    this.levelEl = document.getElementById('level') as HTMLSpanElement;
+    this.linesEl = document.getElementById('lines') as HTMLSpanElement;
     this.resetScore();
   }
 
   clearLineBonus(clearedLines: number) {
     this.score += 100 * clearedLines;
     this.lines += clearedLines;
-    if(this.lines % 10 === 0) {
-      this.level++;
-    }
+    this.level = Math.floor(this.lines / 10) + 1;
     this.updateScore();
   }
 
@@ -36,5 +38,7 @@ export class Score {
 
   private updateScore() {
     this.scoreEl.innerText = this.score.toString();
+    this.levelEl.innerText = this.level.toString();
+    this.linesEl.innerText = this.lines.toString();
   }
 }
